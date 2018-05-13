69402ce48021e787b93638bdcca8d8172b7dfae6
gulhermecoelho <guilhermecoelho2@gmail.com>
2018-05-13 18:54:01 +0100

    first commit

--- SevenSegmentsLed/SevenSegmentsLed.cpp
+++ SevenSegmentsLed/SevenSegmentsLed.cpp
@@ -5,6 +5,81 @@
  Editor:	http://www.visualmicro.com
 */
 
+#include "Arduino.h"
 #include "SevenSegmentsLed.h"
+
+
+SevenSegmentsLed::SevenSegmentsLed(int pinA, int pinB, int pinC, int pinD, int pinE, int pinF, int pinG, int pinDot)
+{
+	pinMode(pinA, OUTPUT);
+	pinMode(pinB, OUTPUT);
+	pinMode(pinC, OUTPUT);
+	pinMode(pinD, OUTPUT);
+	pinMode(pinE, OUTPUT);
+	pinMode(pinF, OUTPUT);
+	pinMode(pinG, OUTPUT);
+	pinMode(pinDot, OUTPUT);
+
+	_pinA = pinA;
+	_pinB = pinB;
+	_pinC = pinC;
+	_pinD = pinD;
+	_pinE = pinE;
+	_pinF = pinF;
+	_pinG = pinG;
+	_pinDot = pinDot;
+}
+
+void SevenSegmentsLed::NumberZero(int hasDot) {
+	WriteDigits(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW, hasDot);
+}
+
+void SevenSegmentsLed::NumberOne(int hasDot) {
+	WriteDigits(LOW, HIGH, HIGH, LOW, LOW, LOW, LOW, hasDot);
+}
+
+void SevenSegmentsLed::NumberTwo(int hasDot) {
+	WriteDigits(HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH, hasDot);
+}
+
+void SevenSegmentsLed::NumberThree(int hasDot) {
+	WriteDigits(HIGH, HIGH, HIGH, HIGH, LOW, LOW, HIGH, hasDot);
+}
+
+void SevenSegmentsLed::NumberFour(int hasDot) {
+	WriteDigits(LOW, HIGH, HIGH, LOW, LOW, HIGH, HIGH, hasDot);
+}
+
+void SevenSegmentsLed::NumberFive(int hasDot) {
+	WriteDigits(HIGH, LOW, HIGH, HIGH, LOW, HIGH, HIGH, hasDot);
+}
+
+void SevenSegmentsLed::NumberSix(int hasDot) {
+	WriteDigits(HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH, hasDot);
+}
+
+void SevenSegmentsLed::NumberSeven(int hasDot) {
+	WriteDigits(HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW, hasDot);
+}
+
+void SevenSegmentsLed::NumberEight(int hasDot) {
+	WriteDigits(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, hasDot);
+}
+
+void SevenSegmentsLed::NumberNine(int hasDot) {
+	WriteDigits(HIGH, HIGH, HIGH, HIGH, LOW, HIGH, HIGH, hasDot);
+}
+
+
+void SevenSegmentsLed::WriteDigits(int pinA, int pinB, int pinC, int pinD, int pinE, int pinF, int pinG, int pinDot) {
+	digitalWrite(_pinA, pinA);
+	digitalWrite(_pinB, pinB);
+	digitalWrite(_pinC, pinC);
+	digitalWrite(_pinD, pinD);
+	digitalWrite(_pinE, pinE);
+	digitalWrite(_pinF, pinF);
+	digitalWrite(_pinG, pinG);
+	digitalWrite(_pinDot, pinDot);
+}
 
 
--- SevenSegmentsLed/SevenSegmentsLed.h
+++ SevenSegmentsLed/SevenSegmentsLed.h
@@ -5,15 +5,39 @@
  Editor:	http://www.visualmicro.com
 */
 
+
 #ifndef _SevenSegmentsLed_h
 #define _SevenSegmentsLed_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
-#else
-	#include "WProgram.h"
+#include "Arduino.h"
+
+class SevenSegmentsLed {
+public:
+	SevenSegmentsLed(int pinA, int pinB, int pinC, int pinD, int pinE, int pinF, int pinG, int pinDot);
+	void NumberZero(int hasDot);
+	void NumberOne(int hasDot);
+	void NumberTwo(int hasDot);
+	void NumberThree(int hasDot);
+	void NumberFour(int hasDot);
+	void NumberFive(int hasDot);
+	void NumberSix(int hasDot);
+	void NumberSeven(int hasDot);
+	void NumberEight(int hasDot);
+	void NumberNine(int hasDot);
+
+private:
+	int _pinA;
+	int _pinB;
+	int _pinC;
+	int _pinD;
+	int _pinE;
+	int _pinF;
+	int _pinG;
+	int _pinDot;
+
+	void WriteDigits(int pinA, int pinB, int pinC, int pinD, int pinE, int pinF, int pinG, int pinDot);
+
+
+};
+
 #endif
-
-
-#endif
-
