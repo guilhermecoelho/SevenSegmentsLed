0769aff09b017e92f38ec68a6ce9c9d6366c8de8
gulhermecoelho <guilhermecoelho2@gmail.com>
2018-05-13 18:47:37 +0100

    Add project files.

---
+++ SevenSegmentsLed/SevenSegmentsLed.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		SevenSegmentsLed.cpp
+ Created:	5/13/2018 6:47:33 PM
+ Author:	guilherme-lenovo
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "SevenSegmentsLed.h"
+
+
---
+++ SevenSegmentsLed/SevenSegmentsLed.h
@@ -0,0 +1,19 @@
+/*
+ Name:		SevenSegmentsLed.h
+ Created:	5/13/2018 6:47:33 PM
+ Author:	guilherme-lenovo
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _SevenSegmentsLed_h
+#define _SevenSegmentsLed_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
